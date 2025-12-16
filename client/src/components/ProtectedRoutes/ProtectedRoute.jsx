a52d8ff91fbf757737a7f4ae26c5936317b7e166
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2025-12-16 16:26:50 +0530

    Add console logs in ProtectedRoute component
    Added console logs to track authentication flow.

--- client/src/components/ProtectedRoutes/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoutes/ProtectedRoute.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => { 
+  console.log("start")
   const isAuthenticated = document.cookie
     .split(';')
     .some((cookie) => cookie.trim().startsWith('AccessToken='));
 
+  console.log("end : ",isAuthenticated)
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
