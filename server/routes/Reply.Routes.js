ede1e42c601c5ceaeaa790749876a734c8768d95
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2026-01-25 00:39:05 +0530

    Fix import case for validate middleware

--- server/routes/Reply.Routes.js
+++ server/routes/Reply.Routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { fetchReplies, submitReply } from '../controllers/Reply.Controller.js';
 import { verifyJwt } from '../middlewares/Auth.Middleware.js';
 import { apiLimiter } from '../middlewares/RateLimit.Middleware.js';
-import { validate } from '../middlewares/validate.middleware.js';
+import { validate } from '../middlewares/Validate.Middleware.js';
 import { submitReplySchema, solutionIdSchema } from "../validation/Reply.Validation.js";
 
 const router = express.Router();
